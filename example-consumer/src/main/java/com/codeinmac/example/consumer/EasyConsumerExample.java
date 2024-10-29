1132b2d4d50ef88831bbd649870252b11a9f6fcb
FangZiyang <fangziyangryan@gmail.com>
2024-10-29 17:23:21 -0400

    add comment CN TO EN

--- example-consumer/src/main/java/com/codeinmac/example/consumer/EasyConsumerExample.java
+++ example-consumer/src/main/java/com/codeinmac/example/consumer/EasyConsumerExample.java
@@ -12,8 +12,8 @@ public class EasyConsumerExample {
     public static void main(String[] args) {
         UserService userService = ServiceProxyFactory.getProxy(UserService.class);
         User user = new User();
-        user.setName("yupi");
-        // 调用
+        user.setName("Terminator-800");
+        //
         User newUser = userService.getUser(user);
         if (newUser != null) {
             System.out.println(newUser.getName());
